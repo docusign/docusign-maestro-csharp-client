77f8da97b3e196c0741603951e7e9c4a0068b765
root <root@devcenteradmin.docusigntest.com>
2024-05-28 23:39:17 -0700

    Version 1.0.0-rc3-v1.0.0-1.0.4 release

--- sdk/src/DocuSign.Maestro/Client/Configuration.cs
+++ sdk/src/DocuSign.Maestro/Client/Configuration.cs
@@ -26,7 +26,7 @@ namespace DocuSign.Maestro.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.0-rc2";
+        public const string Version = "1.0.0-rc3";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
